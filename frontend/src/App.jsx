2f6890410757cf6cf48e5ac1ead2ed552e5b8daf
JawadHaider019 <JawadHaider019>
2026-02-23 16:15:48 +0500

    added heatmap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,65 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
-// import Login from './pages/Login'
-import Product from './pages/Product'
+import Clarity from "@microsoft/clarity";
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
 import Deal from './pages/Deal';
-import Cart from './pages/Cart'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Blog from './pages/Blog'
-import BlogPost from './pages/BlogPost'
-import FaqBot from './components/FaqBot'
-import { ToastContainer } from 'react-toastify'
-import Footer from './components/Footer'
-import 'react-toastify/dist/ReactToastify.css'
-import Favicon from './components/Favicon' 
-import { assets } from './assets/assets' // Import your assets
+import Cart from './pages/Cart';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import FaqBot from './components/FaqBot';
+import { ToastContainer } from 'react-toastify';
+import Footer from './components/Footer';
+import 'react-toastify/dist/ReactToastify.css';
+import Favicon from './components/Favicon';
+import { assets } from './assets/assets';
 
 // Logo Component for SEO
 const LogoSEO = () => {
   return (
-    <div style={{ 
-      position: 'absolute', 
-      left: '-9999px', 
+    <div style={{
+      position: 'absolute',
+      left: '-9999px',
       top: '-9999px',
       width: '112px',
       height: '112px'
     }}>
-      <img 
-        src={assets.logo} 
-        alt="Pure Clay - Pakistan's Organic Foods Brand" 
-        width="112" 
-        height="112" 
+      <img
+        src={assets.logo}
+        alt="Pure Clay - Pakistan's Organic Foods Brand"
+        width="112"
+        height="112"
       />
     </div>
   );
 };
 
 const App = () => {
+  const location = useLocation();
+
+  // Initialize Clarity once
+  useEffect(() => {
+    Clarity.init("vlucuvw2el");
+  }, []);
+
+  // Track route changes (important for SPA)
+  useEffect(() => {
+    Clarity.event("page_view", {
+      path: location.pathname
+    });
+  }, [location]);
+
   return (
     <div className="px-4">
       <Favicon />
+
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -58,7 +74,7 @@ const App = () => {
         style={{
           '--toastify-color-success': '#000000',
           '--toastify-text-color-success': '#ffffff',
-          '--toastify-color-error': '#dc2626', 
+          '--toastify-color-error': '#dc2626',
           '--toastify-text-color-error': '#ffffff',
           '--toastify-color-progress-success': '#10b981',
           '--toastify-color-progress-error': '#fca5a5',
@@ -70,29 +86,28 @@ const App = () => {
         }}
       />
 
-      <Navbar/>
+      <Navbar />
+
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
         <Route path="/deal/:dealId" element={<Deal />} />
         <Route path="/collection/product/:productId" element={<Product />} />
-        {/* <Route path='/login' element={<Login/>}/> */}
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/blog' element={<Blog />} />
         <Route path="/blog/:id" element={<BlogPost />} />
-        <Route path='/orders' element={<Orders/>}/>
-        <Route path='/place-order' element={<PlaceOrder/>}/>
-        
-        {/* ✅ ADD LOGO ROUTE FOR SEO */}
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
         <Route path='/images/logo.png' element={<LogoSEO />} />
       </Routes>
-      <Footer/>
+
+      <Footer />
       <FaqBot />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
